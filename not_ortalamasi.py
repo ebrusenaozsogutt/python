44d9c017a101460d9a2bb27dff790998cdd17622
ebbrusena <ebrusenaozsogut@gmail.com>
2025-10-12 22:15:59 +0300

    ortalama eklendi

---
+++ not_ortalamasi.py
@@ -0,0 +1,7 @@
+midterm_exam_score = input("enter the midterm exam score:")
+final_exam_score=input("enter the final exam score:")
+average_score =(float(midterm_exam_score) + float(final_exam_score))/2
+
+result=f"your midterm exam score is {midterm_exam_score}\nyour final exam score is {final_exam_score}\nyour average score is {average_score}"
+
+print(result)
---
+++ toplam.py
@@ -0,0 +1,6 @@
+num1=int(input("birinci sayıyı giriniz:"))
+
+num2=int(input("ikinci sayıyı giriniz: "))
+sum=num1+num2
+print("toplam:",sum)
+
